0e461f6be8b6efcc1233330b8d22b09179dc94b2
Gennadii <gendalfiori@gmail.com>
2020-04-23 15:49:34 +0200

    Changes in README.md, strong usage of config

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # InstaPostScheduler
+
+Contents of **config.json**:
+```
+{
+  "login": "login",
+  "password": "password",
+  "text_file": "path/to/text/file.txt",
+  "images_dir": "path/to/image/direcory",
+  "time_sleep": "6"
+}
+```
+
+Where `time_sleep` means time between posts in minutes (may be less than 0)
--- main.py
+++ main.py
@@ -6,9 +6,22 @@ from instabot import Bot
 with open("config.json", "r") as f:
     data = json.load(f)
 
-i, n = 0, 0
-text = [[]]
+if 'login' not in data or 'password' not in data:
+    raise AttributeError('Login information is needed')
+if 'text_file' not in data:
+    raise AttributeError('text_file must be specified')
+if 'images_dir' not in data:
+    raise AttributeError('images_dir must be specified')
+if 'time_sleep' not in data:
+    raise AttributeError('time_sleep must be specified')
+if float(data['time_sleep']) < 0:
+    raise AttributeError('time_sleep can not be negative!')
 
+
+timer = int(float(data['time_sleep'])*60)
+
+n = 0
+text = [[]]
 with open(data['text_file'], "r") as f:
     lines = f.readlines()
     for line in lines:
@@ -22,16 +35,16 @@ with open(data['text_file'], "r") as f:
 
 filenames = os.listdir(data['images_dir'])
 filenames.sort()
-timer = int(float(data['time_sleep'])*60)
 
 bot = Bot()
 bot.login(username=data['login'],
           password=data['password'])
+n = 0
 while True:
-    i += 1
-    bot.upload_photo(f"{data['images_dir']}/{filenames[i-1]}", caption=text[i-1])
+    n += 1
+    bot.upload_photo(f"{data['images_dir']}/{filenames[n-1]}", caption=text[n-1])
     time.sleep(timer)
-    if i == min(len(text), len(filenames)):
+    if n == min(len(text), len(filenames)):
         break
 
 bot.logout()
