794691a7891f5c6086f5d43f2c598c226bdb4f6b
Gennadii <gendalfiori@gmail.com>
2020-04-23 15:36:11 +0200

    Making use of config.json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
+import os
 import time
+import json
 from instabot import Bot
 
 with open("config.json", "r") as f:
@@ -8,10 +9,9 @@ with open("config.json", "r") as f:
 i, n = 0, 0
 text = [[]]
 
-with open("text.txt", "r") as f:
+with open(data['text_file'], "r") as f:
     lines = f.readlines()
     for line in lines:
-        print(line)
         if line != "\n":
             text[n].append(line.strip())
         if line == "\n":
@@ -20,13 +20,18 @@ with open("text.txt", "r") as f:
             n += 1
     text[n] = '\n'.join(text[n])
 
+filenames = os.listdir(data['images_dir'])
+filenames.sort()
+timer = int(float(data['time_sleep'])*60)
+
 bot = Bot()
 bot.login(username=data['login'],
           password=data['password'])
-
 while True:
-    print(i)
-    bot.upload_photo(f"images/{i}.jpg", caption=text[i-1])
-    time.sleep(2)
-    if i == len(text):
+    i += 1
+    bot.upload_photo(f"{data['images_dir']}/{filenames[i-1]}", caption=text[i-1])
+    time.sleep(timer)
+    if i == min(len(text), len(filenames)):
         break
+
+bot.logout()
